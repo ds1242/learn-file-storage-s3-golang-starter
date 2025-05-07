2e4a431822791557f5520e8871a606ec10d05395
David Shaw <dshaw11@utk.edu>
2025-05-07 12:31:34 -0400

    now sending video data to s3

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"crypto/rand"
+	"encoding/base64"
+	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -31,4 +40,81 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to find the video", err)
+		return
+	}
+	if videoMetaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorized", err)
+		return
+	}
+
+	videoFile, videoHeader, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "file not found", err)
+		return
+	}
+
+	defer videoFile.Close()
+
+	mediaType, _, err := mime.ParseMediaType(videoHeader.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid content type", err)
+		return
+	}
+	
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "File must be an MP4 video", nil)
+		return
+	}
+
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to create temp file", err)
+		return
+	}
+
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, videoFile)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to copy video file", err)
+		return
+	}
+
+	tempFile.Seek(0, io.SeekStart)
+
+	key := make([]byte, 32)
+	_, err = rand.Read(key)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error", err)
+		return
+	}
+
+	fileString := base64.RawURLEncoding.EncodeToString(key) + "."
+	fileType := strings.Split(mediaType, "/")
+	if len(fileType) > 1 {
+		fileString += fileType[1]
+	} else {
+		respondWithError(w, http.StatusUnsupportedMediaType, "invalid file type", fmt.Errorf("invalid file type"))
+		return
+	}
+
+	_, err = cfg.s3Client.PutObject(context.Background(), &s3.PutObjectInput {
+		Bucket: aws.String(cfg.s3Bucket),
+		Key: aws.String(fileString),
+		Body: tempFile,
+		ContentType: aws.String("video/mp4"),
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failed to upload to s3", err)
+		return
+	}
+
+
+
 }
